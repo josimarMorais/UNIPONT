f07c896808be65ccfdf68a485f1fc666452242b1
Mitra <mitra.mmi@gmail.com>
2023-12-11 03:10:28 -0300

    Update studentController.js

--- controllers/users/student/studentController.js
+++ controllers/users/student/studentController.js
@@ -3,7 +3,6 @@
 module.exports = {
     myCourses(req, res) {
       // Lógica para carregar e exibir cursos do aluno
-      // Utilize Aluno.findByPk(req.user.id) ou outra lógica dependendo da autenticação
       res.render('student/mycourses', { layout: 'student' });
     },
   
